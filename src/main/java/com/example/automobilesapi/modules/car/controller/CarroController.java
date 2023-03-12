df415594c4e7c9fa4c8e002bb60de190081d8e69
RafaRato <rafaelcteixeira1@hotmail.com>
2023-03-11 22:25:14 -0300

    allowing all origins on CORS

--- src/main/java/com/example/automobilesapi/modules/car/controller/CarroController.java
+++ src/main/java/com/example/automobilesapi/modules/car/controller/CarroController.java
@@ -11,6 +11,7 @@ import java.net.URI;
 import java.util.List;
 
 
+@CrossOrigin(origins = "*", allowedHeaders = "*")
 @RestController
 @RequestMapping("/api/carro")
 public class CarroController {
--- src/main/java/com/example/automobilesapi/modules/client/controller/ClienteController.java
+++ src/main/java/com/example/automobilesapi/modules/client/controller/ClienteController.java
@@ -8,6 +8,7 @@ import org.springframework.web.bind.annotation.*;
 import java.net.URI;
 import java.util.List;
 
+@CrossOrigin(origins = "*", allowedHeaders = "*")
 @RestController
 @RequestMapping("/api/cliente")
 public class ClienteController {
